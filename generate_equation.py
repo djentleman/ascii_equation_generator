ef7cc95012d2a4dd0e2ce5e9c7663642b6388eba
Todd Perry <todd.perry@myport.ac.uk>
2017-10-07 18:52:04 +0100

    added functionality to handle dividing

--- generate_equation.py
+++ generate_equation.py
@@ -1,13 +1,25 @@
 
 
+def handle_division(instr):
+    segments = instr.split('/')
+    divisor_size = max([len(s) for s in segments])
+    # add padding to segments
+    segments = map(lambda x: ' '*((divisor_size/2)-(len(x)/2)) + x, segments)
+    return ('\n'+('-'*divisor_size)+'\n').join(segments)
 
 
+def handle_bracketless_string(instr):
+    # brackets needs to be handled outside of this - as their size varies based on
+    # the output of their contents
+    # handle division
+    result = handle_division(instr)
+    return result
 
-
-
+def handle_string(instr):
+    return handle_bracketless_string(instr)
 
 def main():
-    test_input = 'x/y'
+    print handle_string('2x*y/z')
      
 
 if __name__ == '__main__':
