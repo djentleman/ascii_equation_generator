9260b5600da7b8c2f8b972ae9296dc77b9d6af51
Todd Perry <todd.perry@myport.ac.uk>
2017-10-08 14:43:34 +0100

    added vertical concatination functionality

--- textblock.py
+++ textblock.py
@@ -14,23 +14,37 @@ class TextBlock():
     def get_height(self):
         return len(self.block)
 
-    def add_padding(self, rows):
+    def add_padding_horizontal(self, rows):
         self.block = ((rows/2)*[([' ']*self.get_width())]) + self.block
         self.block = self.block + ((rows-(rows/2))*[([' ']*self.get_width())])
 
-    def concatinate_block(self, concat_block):
+    def add_padding_veritcal(self, cols):
+        for i in range(self.get_height()):
+            self.block[i] = ((cols/2)*[' ']) + self.block[i] + ((cols-(cols/2))*[' '])
+
+    def concatinate_block_horizontal(self, concat_block):
         offset = self.get_height() - concat_block.get_height()
         # if offset is positive, add rows to concat_block
         if offset < 0:
-            self.add_padding(abs(offset))
+            self.add_padding_horizontal(abs(offset))
         elif offset > 0:
-            concat_block.add_padding(offset)
+            concat_block.add_padding_horizontal(offset)
 
         new_block = []
         # put current block first
         for i in range(self.get_height()):
             new_block.append(self.block[i] + concat_block.block[i])
         self.block = new_block
+
+    def concatinate_block_vertical(self, concat_block):
+        offset = self.get_width() - concat_block.get_width()
+        if offset < 0:
+            self.add_padding_veritcal(abs(offset))
+        elif offset > 0:
+            concat_block.add_padding_vertical(offset)
+
+        self.block = self.block + concat_block.block
+    
         
     def print_block(self):
         for row in self.block:
